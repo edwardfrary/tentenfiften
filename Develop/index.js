c6ea17b0050313d6398da1fc6f99da5d08cd2f82
edwardfrary <edwardfrary@gmail.com>
2021-06-10 04:47:39 -0400

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,51 +1,80 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const writeFile = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = () => {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Enter your name. (Required)",
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your name.');
-                    return false;
+const questions = readmeData => {
+    console.log(`=============================`);
+
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Enter your name. (Required)",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your name.');
+                        return false;
+                    }
                 }
-            }
-        },
-        {
-            type: "input",
-            name: "title",
-            message: "Enter Project Title. (Required)",
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log("Please enter a project title.")
-                    return false;
+            },
+            {
+                type: "input",
+                name: "title",
+                message: "Enter Project Title. (Required)",
+                validate: titleInput => {
+                    if (titleInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter a project title.")
+                        return false;
+                    }
                 }
+            },
+            {
+                type: "input",
+                name: "desc",
+                message: "Enter Project Description. (Required)",
+                validate: descripInput => {
+                    if (descripInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter a project description.")
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "inst",
+                message: "Specific Installation Instructions:",
+            },
+            {
+                type: "input",
+                name: "usage",
+                message: "Usage Information:",
+            },
+            {
+                type: "input",
+                name: "distGuide",
+                message: "Distribution Guidelines:",
+            },
+            {
+                type: "input",
+                name: "testInst",
+                message: "Test Instructions:",
             }
-        },
-        {
-
-        }
-
-    ])
-        .then(answers => {
-            console.log(answers);
-        });
+        ])
 };
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
-
 // TODO: Create a function to initialize app
-function init() { }
-
 // Function call to initialize app
-init();
-questions();
+questions()
+.then(data => {
+    console.log("Readme Generated!")
+    return writeFile(data);
+});
